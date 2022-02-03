8dd3fa5c41a02aa645a1fcfb4767bc276de702e8
Oleg Kovalenko <helg23@gmail.com>
2022-02-03 14:37:10 +0300

    Домашнее задание HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,65 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int i = 0;
+        while (i < storage.length && storage[i]!=null) {
+            storage[i++]=null;
+        }
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        for(int i = 0; i < storage.length; i++) {
+            if (storage[i]==null){
+                storage[i]=resume;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        int i = 0;
+        while (i< storage.length && storage[i]!=null) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+            i++;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        boolean found = false;
+        int i = 0;
+        while (i< storage.length && storage[i]!=null) {
+            if (!found && storage[i].uuid.equals(uuid)){
+                storage[i]=null;
+                found=true;
+            }
+            if (found && i < (storage.length-1)){
+                storage[i]=storage[i+1];
+            }
+            i++;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int resumeCount = 0;
+        while (resumeCount< storage.length && storage[resumeCount]!=null) {
+            resumeCount++;
+        }
+        Resume[] allResumes = new Resume[resumeCount];
+        System.arraycopy(storage,0,allResumes,0,resumeCount);
+        return allResumes;
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        while (size< storage.length && storage[size]!=null) {
+            size++;
+        }
+        return size;
     }
 }
