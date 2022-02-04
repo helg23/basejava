246481afa8a60689a78df4a290d34f7de47e8040
Oleg Kovalenko <helg23@gmail.com>
2022-02-04 10:40:43 +0300

    Правки по замечаниями. Добавил счетчик элементов, заменил while на for

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,37 +4,37 @@
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    int size = 0; //счетчик непустых элементов
+
     void clear() {
-        int i = 0;
-        while (i < storage.length && storage[i] != null) {
-            storage[i++] = null;
+        for (int i = 0; i < this.size; i++) {
+            storage[i] = null;
         }
+        this.size=0;
     }
 
     void save(Resume resume) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = resume;
-                break;
-            }
+        if (this.size == storage.length) {
+            return;  //не добавляем если массив заполнен
         }
+        if (this.get(resume.uuid)!= null){
+            return; //не добавляем если элемент уже есть
+        }
+        storage[this.size++] = resume;
     }
 
     Resume get(String uuid) {
-        int i = 0;
-        while (i < storage.length && storage[i] != null) {
+        for (int i = 0; i < this.size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
-            i++;
         }
         return null;
     }
 
     void delete(String uuid) {
         boolean found = false;
-        int i = 0;
-        while (i < storage.length && storage[i] != null) {
+        for (int i = 0; i < this.size; i++) {
             if (!found && storage[i].uuid.equals(uuid)) {
                 storage[i] = null;
                 found = true;
@@ -42,28 +42,20 @@ public class ArrayStorage {
             if (found && i < (storage.length - 1)) {
                 storage[i] = storage[i + 1];
             }
-            i++;
         }
+        this.size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int resumeCount = 0;
-        while (resumeCount < storage.length && storage[resumeCount] != null) {
-            resumeCount++;
-        }
-        Resume[] allResumes = new Resume[resumeCount];
-        System.arraycopy(storage, 0, allResumes, 0, resumeCount);
+        Resume[] allResumes = new Resume[this.size];
+        System.arraycopy(storage, 0, allResumes, 0, this.size);
         return allResumes;
     }
 
     int size() {
-        int size = 0;
-        while (size < storage.length && storage[size] != null) {
-            size++;
-        }
-        return size;
+        return this.size;
     }
 }
